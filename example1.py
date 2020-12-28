7864ae5f3bda3c4de31433557961719716b31545
CarlosAdir <carlos.adir.leite@gmail.com>
2020-12-28 21:23:10 +0100

    Simplify problem to only a bar

--- example1.py
+++ example1.py
@@ -16,36 +16,31 @@ if __name__ == "__main__":
     l = sp.symbols("l", real=True, positive=True)
     x = sp.Function("x")(t)
     y = sp.Function("y")(t)
-    q = sp.Function("q")(t)
-    u = sp.Function("u")(t)
-    beta = sp.Function("beta")(t)
-    dbeta = sp.diff(beta, t)
+    theta = sp.Function("theta")(t)
 
     ###########################################################################
     #                   Definition of the Reference Frames                    #
     ###########################################################################
-    R0 = FrameReference()  # Reference absolut
+    R0 = FrameReference()  # Inertial frame of reference
     R1 = FrameReference(base=R0, translation=(x, y, 0))
-    R2 = FrameReference(base=R1, translation=(l / 2, 0, 0))
-    R3 = FrameReference(base=R2, rotation=(beta, (0, 0, 1)))
+    R2 = FrameReference(base=R1, rotation=(theta, "z"))
 
     print(R0)
     print(R1)
     print(R2)
-    print(R3)
 
     ###########################################################################
     #                            Bar calculations                             #
     ###########################################################################
     if 1:
-        mass_bar = sp.symbols("m2")
+        mass_bar = sp.symbols("m")
         # CM_bar = (0, 0, 0)
         II_bar = np.array([[0, 0, 0],
                            [0, 1, 0],
                            [0, 0, 1]])
-        II_bar = II_bar * sp.Rational(1, 12) * m_bar * l**2
+        II_bar = II_bar * sp.Rational(1, 12) * mass_bar * l**2
 
-        bar = Objet(base=R3, name="bar")
+        bar = Objet(base=R2, name="bar")
         bar.set_mass(mass_bar)
         # bar.set_CM(CM_bar)
         bar.set_II(II_bar)
