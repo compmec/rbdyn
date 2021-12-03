cb125ae06b28acacdb888d5c4d6bb752626696ed
Carlos Adir <carlos.adir.leite@gmail.com>
2021-12-03 14:02:53 -0300

    Change tests calling name to Verify
    Before we hade VerifyFunction, we created a class Verify to store all the static functions to use like Verify.Function

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -1,61 +1,61 @@
 import pytest
 import numpy as np
-import compmec.rbdyn.__validation__ as validation
+from compmec.rbdyn.__validation__ import Verify
 
 
-def getRandomUnit3DVector():
+def getRandomUnitVector3D():
     v = np.random.rand(3)
     norm_v = np.linalg.norm(v)
     return v / norm_v
 
 
 @pytest.mark.dependency()
-def test_Verify3DVector():
+def test_VerifyVector3D():
     u = np.zeros(3)
-    validation.Verify3DVector(u)
+    Verify.Vector3D(u)
 
     u = np.random.rand(3)
-    validation.Verify3DVector(u)
+    Verify.Vector3D(u)
 
 
 @pytest.mark.dependency()
-def test_Verify3DTensor():
+def test_VerifyTensor3D():
     X = np.zeros((3, 3))
-    validation.Verify3DTensor(X)
+    Verify.Tensor3D(X)
 
     X = np.eye(3)
-    validation.Verify3DTensor(X)
+    Verify.Tensor3D(X)
 
     X = np.random.rand(3, 3)
-    validation.Verify3DTensor(X + X.T)
+    Verify.Tensor3D(X + X.T)
 
 
-@pytest.mark.dependency(depends=["test_Verify3DTensor"])
+@pytest.mark.dependency(depends=["test_VerifyTensor3D"])
 def test_VerifyAntiSymmetricMatrix():
     X = np.zeros((3, 3))
-    validation.VerifyAntiSymmetricMatrix(X)
+    Verify.AntiSymmetricMatrix(X)
 
     X = np.random.rand(3, 3)
     Xsym = (X + X.T) / 2
     Xant = X - Xsym
-    validation.VerifyAntiSymmetricMatrix(Xant)
+    Verify.AntiSymmetricMatrix(Xant)
 
     with pytest.raises(ValueError):
-        validation.VerifyAntiSymmetricMatrix(Xsym)
+        Verify.AntiSymmetricMatrix(Xsym)
 
 
-@pytest.mark.dependency(depends=["test_Verify3DVector"])
-def test_VerifyUnit3DVector():
-    validation.VerifyUnit3DVector((1, 0, 0))
-    validation.VerifyUnit3DVector((0, 1, 0))
-    validation.VerifyUnit3DVector((0, 0, 1))
-    validation.VerifyUnit3DVector((-1, 0, 0))
-    validation.VerifyUnit3DVector((0, -1, 0))
-    validation.VerifyUnit3DVector((0, 0, -1))
-    validation.VerifyUnit3DVector((0.6, 0.8, 0))
-    validation.VerifyUnit3DVector((0, 0.8, 0.6))
+@pytest.mark.dependency(depends=["test_VerifyVector3D"])
+def test_VerifyUnitVector3D():
+    Verify.UnitVector3D((1, 0, 0))
+    Verify.UnitVector3D((0, 1, 0))
+    Verify.UnitVector3D((0, 0, 1))
+    Verify.UnitVector3D((-1, 0, 0))
+    Verify.UnitVector3D((0, -1, 0))
+    Verify.UnitVector3D((0, 0, -1))
+    Verify.UnitVector3D((0.6, 0.8, 0))
+    Verify.UnitVector3D((0, 0.8, 0.6))
 
     Ntests = 100
     for i in range(Ntests):
-        u = getRandomUnit3DVector()
-        validation.VerifyUnit3DVector(u)
+        u = getRandomUnitVector3D()
+        Verify.UnitVector3D(u)
